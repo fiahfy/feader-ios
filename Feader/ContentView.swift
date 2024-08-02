d3b80b35136f04424da8d00ef758c07ec604c837
fiahfy <d.fiahfy@gmail.com>
2024-08-02 20:53:21 +0900

    Show app version

--- Feader/ContentView.swift
+++ Feader/ContentView.swift
@@ -2,23 +2,18 @@ import SwiftUI
 
 struct ContentView: View {
   var body: some View {
-//    VStack {
-//      Image(systemName: "globe")
-//        .imageScale(.large)
-//        .foregroundStyle(.tint)
-//      Text("Hello, world!")
-//    }
-//    .padding()
     TabView {
-      HomeView()
-        .tabItem {
-          Image(systemName: "house")
-        }
+      Group {
+        HomeView()
+          .tabItem {
+            Image(systemName: "house")
+          }
 
-      SettingsView()
-        .tabItem {
-          Image(systemName: "gearshape")
-        }
+        SettingsView()
+          .tabItem {
+            Image(systemName: "gearshape")
+          }
+      }
     }
   }
 }
--- Feader/SettingsView.swift
+++ Feader/SettingsView.swift
@@ -1,8 +1,34 @@
 import SwiftUI
 
 struct SettingsView: View {
+  private var appVersion: String {
+    Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "Unknown"
+  }
+
+  private var buildVersion: String {
+    Bundle.main.infoDictionary?["CFBundleVersion"] as? String ?? "Unknown"
+  }
+
   var body: some View {
-    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    NavigationView {
+      Form {
+        Section {
+          HStack {
+            Text("Version")
+              .font(.body)
+              .foregroundColor(.primary)
+
+            Spacer()
+
+            Text("\(appVersion) (\(buildVersion))")
+              .font(.body)
+              .foregroundColor(.secondary)
+          }
+        }
+      }
+      .navigationTitle("Settings")
+      .navigationBarTitleDisplayMode(.inline)
+    }
   }
 }
 
